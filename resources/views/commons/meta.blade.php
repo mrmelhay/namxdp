a52d9b543c36a0076d233d4b07857d0fe40e0c7a
ideveloper <bahrom1982@gmail.com>
2018-09-17 19:48:24 +0500

    Commit

--- resources/views/commons/meta.blade.php
+++ resources/views/commons/meta.blade.php
@@ -4,9 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{!! config('app.name', 'Laravel') !!}</title>
-
     <link rel="apple-touch-icon" href="{{ asset('images/apple-touch-icon.png') }}">
     <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">
     <!-- Stylesheets -->
